2f149dc557f126328442462686f5b7fb6c408c73
Erik Nilsen <enilsen16@live.com>
2015-02-05 23:53:09 -0800

    code works tests fail :cry:

--- test/logic_test.js
+++ test/logic_test.js
@@ -2,5 +2,9 @@ var expect = require('chai').expect;
 var logic = require('../lib/logic');
 
 describe('logic', function() {
-
+  it('is a valid gif', function() {
+    var yeah = logic.giphy('yeah');
+    expect(yeah).to.contain('http:');
+    expect(yeah).to.contain('.gif');
+  });
 });
