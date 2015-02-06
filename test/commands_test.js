46d61b0d8df7b79512d3f46726878feab522ca11
Erik Nilsen <enilsen16@live.com>
2015-02-05 19:55:50 -0800

    add env variables and set up test file

--- test/commands_test.js
+++ test/commands_test.js
@@ -1,4 +1,6 @@
 var expect = require('chai').expect;
-var command = require('../lib/commands');
+var commands = require('../lib/commands');
 
-describe('commands', function() {});
+describe('commands', function() {
+
+});
