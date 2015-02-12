b89924d7405d85c3e8517ead0c239632e2064538
Erik Nilsen <enilsen16@live.com>
2015-02-12 12:33:56 -0800

    add uptime command

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var irc = require('twitch-irc');
 var image = require('./lib/image');
 var protection = require('./lib/protection');
 var dotenv = require('dotenv');
+var moment = require('moment');
 dotenv.load(); // For enviroment variables
 
 var client = new irc.client({
@@ -21,8 +22,7 @@ var client = new irc.client({
 });
 
 client.connect();
-
-// Your events..
+var timeA = moment();
 client.addListener('chat', function (channel, user, message) {
   if (message.indexOf('!hello') === 0) {
     client.say(channel, 'Hey '+user.username+'! How you doing? Kappa');
@@ -32,7 +32,7 @@ client.addListener('chat', function (channel, user, message) {
       client.say(channel, url);
     });
   }
-  if((client.utils.symbols(message) > 0) && (user.special.length <= 0 || user.special.contains('turbo'))) {
+  if((client.utils.symbols(message) > 0) && (user.special.length === 0 || user.special.contains('turbo'))) {
     client.timeout(channel, user.username, 5);
     client.say(channel, 'Hey '+user.username+"! Please don't spam");
   }
@@ -40,6 +40,15 @@ client.addListener('chat', function (channel, user, message) {
     client.say(channel, "1. A robot may not injure a human being or, through inaction, allow a human being to come to harm.");
     client.say(channel, "2. A robot must obey any orders given to it by human beings, except where such orders would conflict with the First Law.");
     client.say(channel, "3. A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.");
+  }
+  if(message.length >= 250 && (user.special.length === 0 || user.special.contains('turbo'))) {
+    client.timeout(channel, user.username, 5);
+    client.say(channel, user.username + " that message is way too long");
+  }
+  if(message.indexOf('!uptime') === 0) {
+    var timeB = moment();
+    var uptime = timeB.diff(timeA, 'minutes');
+    client.say(channel, "I have been running for about " +  uptime + " minutes");
   }
 });
 
