8a537bb3357e578df210b1dadb3d14c72ddec722
Erik Nilsen <enilsen16@live.com>
2015-02-18 16:07:15 -0800

    fix test

--- test/image_test.js
+++ test/image_test.js
@@ -5,7 +5,6 @@ describe('image', function() {
   it('is a valid gif', function(done) {
     image.giphy("whassup", function(url) {
       expect(url).to.contain('http:');
-      expect(url).to.not.be.undefined();
       expect(url).to.contain('.gif');
       done();
     });
